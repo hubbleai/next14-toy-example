00ea4415beff533dc8c44cc6d16037211bd93d7e
Kailash Anantha <kailash@polymer.fyi>
2024-01-18 21:12:00 +0530

    Adding CC implementation

--- app/page.js
+++ app/page.js
@@ -1,6 +1,21 @@
-import Image from 'next/image'
+'use client';
+import react from 'react';
+import Image from 'next/image';
+import { CarbonConnect, IntegrationName } from 'carbon-connect';
+import { generateAccessToken } from 'carbon-connect-js';
 
 export default function Home() {
+  const tokenFetcher = async () => {
+    const res = await generateAccessToken({
+      apiKey: 'your_api_key',
+      customerId: 'your_customer_id',
+    });
+
+    if (res.status === 200) {
+      return res.data;
+    }
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -8,6 +23,136 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.js</code>
         </p>
+        <CarbonConnect
+          orgName="Rubber"
+          brandIcon="/assets/images/icon-integration.png"
+          tokenFetcher={tokenFetcher}
+          tags={{
+            appType: 'chatbot',
+            appId: 10,
+            appIds: [11, 12, 13],
+            appString: ['hello', 'world'],
+            appVersion: '1.1.0',
+            appDescription: 'Chatbot for Rubber',
+          }}
+          // maxFileSize={20000000}
+          enabledIntegrations={[
+            {
+              id: 'LOCAL_FILES',
+              chunkSize: 1500,
+              overlapSize: 20,
+              maxFileSize: 20000000,
+              allowMultipleFiles: true,
+              maxFilesCount: 50,
+              filePickerMode: 'BOTH',
+              allowedFileTypes: [
+                {
+                  extension: 'csv',
+                  chunkSize: 1200,
+                  overlapSize: 120,
+                },
+                {
+                  extension: 'txt',
+                  chunkSize: 1599,
+                  overlapSize: 210,
+                },
+                {
+                  extension: 'pdf',
+                },
+                {
+                  extension: 'docx',
+                },
+                {
+                  extension: 'pptx',
+                },
+                {
+                  extension: 'md',
+                },
+              ],
+            },
+            {
+              id: 'NOTION',
+              chunkSize: 150,
+              overlapSize: 15,
+              // allowMultiple: true,
+            },
+            {
+              id: 'WEB_SCRAPER',
+              chunkSize: 1500,
+              overlapSize: 20,
+              recursionDepth: 3,
+              maxPagesToScrape: 50,
+            },
+            {
+              id: 'GMAIL',
+              chunkSize: 1000,
+              overlapSize: 35,
+              allowMultiple: true,
+            },
+            {
+              id: 'GOOGLE_DRIVE',
+              chunkSize: 1000,
+              overlapSize: 35,
+              allowMultiple: true,
+              generateSparseVectors: true,
+            },
+            {
+              id: 'INTERCOM',
+              chunkSize: 1500,
+              overlapSize: 20,
+            },
+            {
+              id: 'DROPBOX',
+              chunkSize: 1500,
+              overlapSize: 20,
+            },
+            {
+              id: 'ONEDRIVE',
+              chunkSize: 1500,
+              overlapSize: 20,
+            },
+            {
+              id: 'BOX',
+              chunkSize: 1500,
+              overlapSize: 20,
+            },
+            {
+              id: 'S3',
+              chunkSize: 1500,
+              overlapSize: 20,
+              skipEmbeddingGeneration: false,
+            },
+            {
+              id: 'SHAREPOINT',
+              chunkSize: 1500,
+              overlapSize: 20,
+              skipEmbeddingGeneration: false,
+            },
+            {
+              id: 'ZENDESK',
+              chunkSize: 1500,
+              overlapSize: 20,
+              skipEmbeddingGeneration: false,
+            },
+          ]}
+          onSuccess={(data) =>
+            console.log('Data on Success (Generic): ', data, auth0User?.email)
+          }
+          onError={(error) =>
+            console.log('Data on Error (Generic): ', error, auth0User?.email)
+          }
+          primaryBackgroundColor="#525252"
+          primaryTextColor="#000000"
+          secondaryBackgroundColor="#f2f2f2"
+          secondaryTextColor="#000000"
+          allowMultipleFiles={true}
+          // open={showDataSourcesModal}
+          // setOpen={setShowDataSourcesModal}
+          chunkSize={1500}
+          overlapSize={20}
+          // alwaysOpen={true}
+          navigateBackURL="https://www.google.com"
+        ></CarbonConnect>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -109,5 +254,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
